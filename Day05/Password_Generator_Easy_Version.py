1c2b17202ded382fd9c0417eee4ffd8ce08dc91c
esraakblk <esraakbulak@gmail.com>
2024-10-29 16:00:47 +0300

    Easy password generation project added.

--- Day05/Password_Generator_Easy_Version.py
+++ Day05/Password_Generator_Easy_Version.py
@@ -1,5 +1,6 @@
 import random
 
+# Lists of possible characters for the password
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -9,15 +10,18 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Initializing an empty password string
+password = []
 
-password = ""
-
-for char in range(1, nr_letters + 1):
+# Adding random letters to the password
+for char in range(0, nr_letters):
     password += random.choice(letters)
 
+# Adding random numbers to the password
 for char in range(0, nr_numbers):
     password += random.choice(numbers)
 
+# Adding random symbols to the password
 for char in range(0, nr_symbols):
     password += random.choice(symbols)
 
